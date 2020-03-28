eae819fd5834314120983f4c8a57da812221d911
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-03-28 20:12:20 +0100

    Added filtering data and sending request to firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,6 +7,37 @@ import Search from './Search';
 const Ingredients =() => {
 
     const [userIngredients, setUserIngredients] =  useState([]);
+
+    //useEffect wykona się zawsze, gdy ten komponent zostanie zrenderowany i NA KOŃCU CYKLU - PO WSZYSTKICH FUNKCJACH
+    useEffect(() => {
+        fetch("https://react-hooks-df071.firebaseio.com/ingredients.json")
+            .then(response => response.json())
+            .then(responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount
+                    })
+                }
+                setUserIngredients(loadedIngredients);
+            })
+        //useEffect ma drugi argument - tablicę z ZALEŻNOŚCIAMI FUNKCJI (TEJ WEWNĄTRZ USEEFFECT) I TYLKO WTEDY, GDY ZALEŻNOŚCI
+        // ULEGNA ZMIANIE, FUNKCJA ODPALI SIĘ PONOWNIE. DZIEKI TEMU MOŻNA DECYDOWAĆ JAK CZĘSTO FUNKCJA BĘDZIE SIĘ URUCHAMIAĆ.
+        //DOMYŚLNIE JEST TO PO KAŻDYM CYKLU ŁADOWANIA KOMPONENTU (RENDER)
+        //UMIESZCZENIE PUSTEJ TABLICY SPOWODUJE, ŻE USEEFFECT URUCHOMI SIĘ TYLKO RAZ - PO PIERWSZYM ZAŁADOWANIU STRONY (ZACHOWUJE SIĘ W TAKIEJ
+        //KONFIGURACJI IDENTYCZNIE, CO COMPONENTDIDMOUNT - URUCHAMIA SIE TYLKO RAZ
+    }, []);
+
+    //ta funkcja filtruje wyszukiwane składniki
+    //używam tu useCallback ponieważ gdy go nie było, komponent się renderował (ponieważ ładowała się lista wprowadzonych wczesniej składnikow)
+    //i funkcja przekazywała do komponentu search za każdym razem dane
+    //i wtedy odświeżała się wartość funkcji przekazanej jako drugi parametr w zależnościach w useEffect. useCallback też posiada tablicę
+    //z zaleznościami, ale w tym przypadku wykona się tylko raz.
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+        setUserIngredients(filteredIngredients);
+    }, []);
 
     //Ingredient to wprowadzone przez inputy składniki w ingredientForm, a fukcja set tworzy tablice z pierwotnego
     //stanu składników i obiektu zawierającego unikalny id i listę skopiowanych składników wprowadzonych w formularzu
@@ -33,7 +64,7 @@ const Ingredients =() => {
 
     const removeIngredientHandler = ingredientId => {
         setUserIngredients(prevIngr => prevIngr.filter(ingredients => ingredients.id !== ingredientId));
-    }
+    };
 
   return (
     <div className="App">
@@ -41,7 +72,7 @@ const Ingredients =() => {
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         {/*tu idą pierwotne ingrediencje*/}
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
       </section>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+    //tutaj DESTRUKTURYZUJĘ obiekt, który stanowi funkcja onLoadingredients. dzięki temu mogę niżej zapisać zamiast
+    //props.onLoad... samo onLoad... i poza tym dodać tylko tą funkcję do zależności w useeffect bez angażowania do tego propsów
+    const { onLoadIngredients } = props;
+    const [enteredFilter, setEnteredFilter] = useState('');
+
+
+    useEffect(() => {
+        //tutaj będę filtrował dane, które pójdą do firebase z zapytaniem. jeśli nie została wprowadzona wartość, wtedy query będzie pustym
+        // stringiem. Jeżeli nie, to posortuj po tytułach i wyświetl, jeśli składnik zgadza się z wprowadzoną w wyszukiwarce wartością
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-df071.firebaseio.com/ingredients.json' + query)
+            .then(response => response.json())
+            .then(responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount
+                    })
+                }
+                //onloadIngredients to funkcja, którą oczekuje komponent search zawarty w Ingredients, tam ją obsłużę
+                onLoadIngredients(loadedIngredients);
+            })
+        //chcę, żeby useEffect wykonało się ZA KAŻDYM RAZEM, gdy zmieni się wprowadzona wartość - wyszukiwany składnik, LUB onloadingredients
+        // dlatego umieszczam te rzeczy jako zależność useEffect
+    }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+              type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
         </div>
       </Card>
     </section>
