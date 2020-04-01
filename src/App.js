b6cfbca14da5318794d03e5dc7827c2a3f9732fb
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-04-01 18:14:27 +0200

    Added authentication - used Context.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth'
+import {AuthContext} from './auth-context';
 
 const App = props => {
-  return <Ingredients />;
+
+  const authContext = useContext(AuthContext);
+
+  //domyślnie treścią strony jest autoryzacja, ale gdy uda sie zalogować to wtedy zamieniam na formularz składników i zwracam zawartość
+  //tej zmiennej
+  let content = <Auth/>;
+
+  if (authContext.isAuth) {
+    content = <Ingredients/>
+  }
+
+  return content;
 };
 
 export default App;
